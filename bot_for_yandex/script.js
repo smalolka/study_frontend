df59c42fa6773fef0a5a867cb46d554f6dfeff9a
smalolka <71058815+smalolka@users.noreply.github.com>
2020-09-13 06:42:17 +0300

    13.09.2020

---
+++ bot_for_yandex/script.js
@@ -0,0 +1,38 @@
+// ==UserScript==
+// @name         New Userscript
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://yandex.ru/*
+// @grant        none
+// ==/UserScript==
+
+//
+
+let keywords = ["Гобой","Как звучит флейта","Кларнет"];
+let keyword = keywords[getRandom(0,keywords.length)];
+let text = document.getElementById("text")[0];
+let button;
+
+
+
+if (button != undefined){
+    text.value[0] = keyword;
+    button.dataset.bem.click();
+
+}else{
+    let links = document.links;
+
+    for(let i = 0; i<links.length; i++){
+        if(links[i].href.indexOf("xn----7sbab5aqcbiddtdj1e1g.xn--p1ai") != -1){
+            console.log("Ссылка найдена"+links[i]);
+            links[i].click();
+            break;
+        }
+    }
+}
+
+function getRandom(min,max){
+    return Math.floor(Math.random()*(max-min)+min);
+}
